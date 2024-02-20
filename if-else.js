43853413b00e94a1cc9b01b8ccb5058ee836fec3
coderstrustsozib <coderstrustbdsozib@gmail.com>
2024-02-20 17:36:15 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,9 @@
+const data = ["Rohim", "Korim", "kather"];
+
+const project = {
+    firstName:"John", 
+    lastName:"Doe",
+    age:50,
+    eyeColor:"blue"
+
+};
---
+++ while-loop.js
@@ -0,0 +1,33 @@
+var roastGiven = 0;
+
+
+// while(roastGiven <= 10){
+//     console.log(roastGiven);
+//     console.log("Mama!!, roast den, Please!!");
+//     roastGiven = roastGiven + 3;
+//     // roastGiven += 2;
+//     // roastGiven ++;
+
+// }
+
+// Event number
+
+// while(roastGiven <= 20){
+//     console.log(roastGiven);
+//     console.log("Mama!!, roast den, Please!!");
+//     roastGiven = roastGiven + 2;
+//     // roastGiven += 2;
+//     // roastGiven ++;
+
+// }
+
+// Odd number
+// roastGiven = 1;
+while(roastGiven <= 10){
+    console.log(roastGiven);
+    console.log("Mama!!, roast den, Please!!");
+    roastGiven = roastGiven + 1;
+    // roastGiven += 2;
+    // roastGiven ++;
+
+}
